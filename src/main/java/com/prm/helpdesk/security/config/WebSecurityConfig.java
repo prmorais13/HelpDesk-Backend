1b6ac5019ef446050b6d0188ca73c007784dcc4f
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-03-13 19:13:30 -0300

    Testes com postman

--- src/main/java/com/prm/helpdesk/security/config/WebSecurityConfig.java
+++ src/main/java/com/prm/helpdesk/security/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -42,6 +43,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	public JwtAuthenticationTokenFilter authTokenFilterBean() throws Exception {
 		return new JwtAuthenticationTokenFilter();
+	}
+	
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationManager();
 	}
 	
 	@Override
